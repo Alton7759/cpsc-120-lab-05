3a6dee1993ca580166b1238107b7bcafdfb2a921
Christian Bonilla <Alton77@csu.fullerton.edu>
2022-10-12 13:19:27 -0400

    looks good to me

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,14 @@
-// TODO: Add the required header
+// Christian Alton bonilla
+// CPSC 120-01
+// 2022-09-21
+// Alton77@csu.fullerton.edu
+// @alton7759
+//
+// Lab 01-01
+// Partners: @annavera38
+//
+// prints the change from fahrenheit to celsius
+//
 
 #include <iostream>
 #include <string>
@@ -7,39 +17,23 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments(argv, argv + argc);
   double sum{0};
-  int i = 1;
-  // TODO: Validate that there is at least one command line argument.
-  // If not, print an error message and return a non-zero value.
+  int i{1};
   if (arguments.size() <= 1) {
     std::cout << "error: must supple at least one number\n";
+    return 0;
   }
-  // TODO: Write a for-each loop to sum (add up) all of the command line
-  // arguments.
-  // Use a double or float type so that your program preserves fractional
-  // values.
-  // The loop needs to skip over the command name, which is the first element
-  // of the arguments vector.
-  // Each argument is a std::string. You will need to convert each string into
-  // a number with the std::stod or std::stof function.
   bool skip{true};
   for (const std::string& num : arguments) {
-    if (skip == true) {
+    if (skip) {
       skip = false;
 
     } else {
-      sum = sum + std::stod(arguments.at(i));
+      sum = sum + std::stod(num);
       i++;
     }
-    int s = arguments.size() - 1;
   }
-  std::cout << sum;
-  // TODO: After the loop has finished summing the arguments, calculate the
-  // average of the values. Recall that the average is the total value divided
-  // by the number of values.
-
-  // TODO: Use cout to print out a message of the form
-  // average = *AVERAGE*
-  // on its own line.
-
+  double s = arguments.size() - 1.0;
+  double average = sum / s;
+  std::cout << "average = " << average << "\n";
   return 0;
 }
